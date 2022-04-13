b74b5d5c32daa0b2fd4895f84afb1f0a58159b1b
Lucas Ko <lucasko.tw@gmail.com>
2022-04-13 21:52:15 +0800

    s3

--- startup.sh
+++ startup.sh
@@ -16,5 +16,15 @@ sudo usermod -aG docker $USER
 echo "installing package"
 sudo yum install -y git
 
+# install gh
+VERSION=`curl  "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
+echo $VERSION
+### With wget ###
+wget https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_amd64.tar.gz
+tar xvf gh_${VERSION}_linux_amd64.tar.gz
+sudo cp gh_${VERSION}_linux_amd64/bin/gh /usr/local/bin/
+gh version
+
+
 echo "rebooting"
 sudo reboot
