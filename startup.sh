281ce22874a2426a4d2dda76a82be5f2457993b2
Lucas Ko <lucasko.tw@gmail.com>
2022-05-15 23:43:41 +0800

    sudo usermod -aG docker $USER

--- startup.sh
+++ startup.sh
@@ -6,6 +6,7 @@ sudo snap install amazon-ssm-agent --classic
 echo "installing docker"
 sudo yum update
 sudo yum install -y docker
+sudo usermod -aG docker $USER
 wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
 sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
 sudo chmod -v +x /usr/local/bin/docker-compose
